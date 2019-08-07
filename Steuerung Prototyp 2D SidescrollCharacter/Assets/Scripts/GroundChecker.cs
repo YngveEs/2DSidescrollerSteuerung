7eebc257a21cdc5f752a203a95f24a300af3385f
YngveEs <yngve.raina.essing@gmail.com>
2019-08-07 17:39:21 +0200

    BasicMovement
    Movement, Jump, Fall and GroundCheck

---
+++ Steuerung Prototyp 2D SidescrollCharacter/Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public Vector3 groundCheckOffset;
+    public float groundCheckRadius = 0.02f;
+    public LayerMask groundLayerMask = -1; // -1 = everything
+
+
+
+    
+    public bool IsGrounded()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + groundCheckOffset, groundCheckRadius, groundLayerMask);
+
+        for (int i = 0; i < colliders.Length; i++  )
+        {
+            if (colliders[i].gameObject != gameObject && !colliders[i].isTrigger)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position + groundCheckOffset, groundCheckRadius);
+    }
+}
---
+++ Steuerung Prototyp 2D SidescrollCharacter/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D), typeof(Animator))]
+public class MovementScript : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 20f;
+    public float jumpSpeed = 5f;
+
+
+    private Rigidbody2D rigidB;
+    private Animator anim;
+    private GroundChecker groundCheck;
+
+
+    void Awake()
+    {
+        rigidB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        groundCheck = GetComponent<GroundChecker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0.01f || Input.GetAxis("Horizontal") > 0.01f)
+        {
+            if (groundCheck.IsGrounded())
+                Walk();
+            else
+                Fall();
+        }
+        if (Input.GetButtonDown("Jump") && groundCheck.IsGrounded())
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        }
+    }
+    void Walk()
+    {
+        rigidB.velocity = new Vector2(Input.GetAxis("Horizontal") * walkSpeed, rigidB.velocity.y);
+    }
+
+    void Run()
+    {
+        rigidB.velocity = new Vector2(Input.GetAxis("Horizontal") * runSpeed, rigidB.velocity.y);
+
+    }
+
+    void Jump()
+    {
+        rigidB.velocity = new Vector2(rigidB.velocity.x, jumpSpeed);
+
+    }
+    void Fall()
+    {
+        print("Falling!");
+    }
+}
